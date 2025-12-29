c50b3f405aa1b9ba77bbd2d7f8f77a48bc8d621f
Anvesh-999 <anvesh.a9100@gmail.com>
2025-12-29 15:17:26 +0530

    wheather api app ready

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,29 @@
+import SearchBox from './SearchBox'
+import InfoBox from './InfoBox'
+import { useState } from 'react';
+
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        city:"Hyderabad",
+        feels_like: 20.84,
+        humidity: 50,
+        pressure: 1014,
+        temp: 21.34,
+        temp_max: 21.34,
+        temp_min: 21.34,
+        weather: "clear sky",
+        wind_speed: 1.76,
+    });
+
+    let updateInfo=(newInfo)=>{
+        setWeatherInfo(newInfo);
+    }
+
+    return (
+        <div className="WeatherApp" style={{textAlign:"center"}}>
+            <h1>Weather Widget</h1>
+            <SearchBox updateInfo={updateInfo}/>
+            <InfoBox  info={weatherInfo}/>
+        </div>
+    );
+}
